5404e1729c891422cde1ea19aaaa997d770ace46
7shivg <shivashgaur33@gmail.com>
2024-02-01 20:39:19 +0530

    first commit

---
+++ ques.js
@@ -0,0 +1,418 @@
+// 1.  print hello world 
+console.log("Hello World")
+
+
+// 2. You need to print "Floobyte Intern" on a line, and print "A Transformation in Education" on a new line.
+console.log("Floobyte Intern \n")
+console.log("A Transformation in Education")
+
+// 3. You are given a number stored in a variable, with the name n
+
+// let nums = 50;
+// let N =  prompt("Enter a number :");
+// console.log(nums*N);
+
+/* 4.You are given a number, stored in a variable with the name n. Perform the following operations on the value stored in n:
+
+     1. Multiply the value by 3
+     2. Add 7 to it
+     3. Subtract 10 from it
+    After performing all the above operations, print the updated value*/
+
+    // let num = 50;
+    // let ans = num*3;
+    // ans = ans+7;
+    // ans = ans -10;
+    // console.log(ans);
+
+// 5. squre a number.......
+
+// let i = prompt("enter a number :");
+// console.log(i*i);
+
+
+// 6.cube of a number 
+// let a = 3;
+// let number = prompt("enter a number ");
+// console.log(number**a);
+
+
+// 7.After multiplying the six numbers, we get the following result: 1 * 2 * 3 * 4 * 5 * 6 = 720, which is the required answer
+
+// let arr = [1,2,3,4,5,6,7];
+// let ans1 = arr[0];
+// for(let i =1;i<arr.length;i++){
+//     ans1=ans1*arr[i];
+// }
+// console.log(ans1);
+
+// 8. sum of 5
+
+// let arr = [1,2,3,4,5];
+// let ans1 = arr[0];
+// for(let i = 1;i<arr.length;i++){
+//     ans1= ans1+arr[i];
+// }
+// console.log(ans1);
+
+
+// 9. product of eight 
+
+// let arr1 = [1,2,3,4,5,6,7,8];
+// let ans2 = arr1[0];
+// for(let i = 1;i<arr1.length;i++){
+//     ans2 = ans2*arr1[i];
+// }
+// console.log(ans2);
+
+// 10. double the elements and the sum of them 
+
+// let arr = [1,2,3,4,5];
+// let ans = 0;
+// for(let i = 0;i<arr.length;i++){
+//     ans = ans+(arr[i]*2);
+// }
+// console.log(ans);
+
+// 11. DOUBLE TO TRIPPLE ONE 
+
+
+// let arr = [1, 2, 3, 4];
+// let ans = arr[0] * 2; 
+
+// for (let i = 1; i < arr.length; i++) {
+//     if (i === 1) {
+//         ans += (arr[i] * 2); 
+//     } else if (i === 2) {
+//         ans += (arr[i] * 3);
+//     } else if (i === 3) {
+//         ans += (arr[i] + 4); 
+//     }
+// }
+
+// console.log(ans);
+
+/* 12.You have to perform the following operations on the value stored in N:
+
+1. Multiply the value stored in N by 40
+2. Add 20 to the value stored in N
+3. Subtract 50 from the value stored in N
+
+You are given another number stored in the variable M after all th the operation on the n print which is greater */
+
+// let num = prompt("enetr a number N: ");
+// let num2 = prompt("enter the number M :");
+
+// let numAns = num*40;
+// numAns = numAns+20;
+// numAns = numAns-50;
+
+// if(numAns>num2){
+//     console.log("it is greater ");
+// }else{
+//     console.log("it is smaller ");
+// }
+
+/*13. Seven Numbers 15/01/2024 to 16/01/2024
+Description
+
+You are given 7 numbers A, B, C, D, E, F, G. Find out the product of (A + B + C) and (D + E + F + G).*/
+
+// let arr = [1,2,3,4,5,6,7];
+// let ans1= 0;
+// let ans2 =0;
+// for(let i = 0;i<=2;i++){
+//     ans1 = ans1+arr[i];
+// }
+// for(let i = 3;i<arr.length;i++){
+//     ans2= ans2+arr[i];
+// }
+// console.log(ans1*ans2);
+
+/*14. 
+Given a value of X, find the final value of X, if X is multiplied by 7 twice.*/
+
+// let N = prompt("enter a number :");
+// console.log(N*7*7);
+
+/* 15. Infinity Stones
+  indexes 2 5 and 6   are doubled 1 remains unchanged and 3 4 are tripled */
+
+
+// const input = '1 2 3 4 5 6';
+// const values = input.split(' ').map(Number);
+// const updatedValues = [
+//     values[0],                
+//     values[1] * 2,           
+//     values[2] * 3,            
+//     values[3] * 3,            
+//     values[4] * 2,            
+//     values[5] * 2             
+// ];
+// const sum = updatedValues.reduce((acc, curr) => acc + curr, 0);
+// console.log(sum);
+
+/*16.Update & Compare 15/01/2024 to 16/01/2024
+Description
+
+You are given two numbers, stored in variables with the names num1 and num2
+
+Print the output of the following operation
+
+num1 > num2, this prints true if num1 is greater than num2, else it prints false
+After this operation is performed, another number, stored in a variable with name num3 is added to num1. After this, print the result of the following operation again
+
+num1 > num2, this prints true if num1 is greater than num2, else it prints false
+*/
+// let num1 = 4;
+// let num2 = 8;
+// if(num1>num2){
+//     console.log("true")
+// }else{
+//     console.log("false");
+
+// }
+// let num3 = 5;
+// num1 = num1+num3;
+// if(num1>num2){
+//     console.log("true")
+// }else{
+//     console.log("false");
+
+// }
+/* 17. Compare two Numbers 15/01/2024 to 16/01/2024
+Description
+
+You are given two numbers stored in two variables, with the name, num1 and num2
+
+Print the result of the following operations
+
+1. num1 > num2 -> this operation prints true if num1 is greater than num2, otherwise it prints false
+2. num1 < num2 -> this operations prints true if num1 is smaller than num2, otherwise it prints false
+3. num1 == num2 -> this operation print true if num1 is equal to num2, otherwise it prints false
+You have to print the result of three operations in the order as shown above*/
+
+// let num1 = prompt("num1");
+// let num2 = prompt("num2");
+
+// if(num1==num2){
+//     console.log("true");
+// }
+// else{
+//     console.log("false")
+// }
+// if(num1>num2){
+//     console.log("true");
+// }
+// else{
+//     console.log("false")
+// }
+// if(num2>num1){
+//     console.log("true")
+// }
+// else{
+//     console.log("false")
+// }
+
+
+/* 18. Sum & Compare 15/01/2024 to 16/01/2024
+Description*/ 
+
+// const arr = [2,5,6,8,7];
+// let sum1 = 0;
+// let sum2 = 0;
+
+
+// for(let i = 0;i<=2;i++){
+//     sum1 = sum1+arr[i];
+// }
+// for(let i = 3;i<arr.length;i++){
+//     sum2 = sum2+arr[i];
+// }
+
+// if(sum1>sum2){
+//     console.log("true");
+// }else{
+//     console.log("false");
+// }
+
+//19 <-------------
+// const arr = [1,2,3,4,5,6,7];
+// let sum1 =(arr[0]+arr[1])*arr[2];
+// let sum2 = 0;
+// for(let i = 3;i<arr.length;i++){
+//     sum2 = sum2+arr[i];
+// }
+// if(sum1>sum2){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+
+// 20.  <---------------
+// let l1 = prompt("l1"); 
+// let b1 = prompt("b1"); 
+// let l2 = prompt("l2"); 
+// let b2 = prompt("b2"); 
+
+// let areaRec1 = l1*b1;
+// let areaRec2 = l2*b2;
+
+// let periRec1 = 2*(l1+b1);
+// let periRec2 = 2*(l2+b2);
+
+// if(areaRec1>areaRec2){
+//     console.log("true");
+// }else{
+//     console.log("false");
+// }
+
+// if(periRec1>periRec2){
+//     console.log("true");
+// }else{
+//     console.log("false");
+// }
+
+// 21. cube of n and sqaure of n to be compaired
+
+// let N = prompt("N");
+// let M= prompt("M");
+
+// let cube = N*N*N;
+// let square = M*M;
+
+// if(cube>square){
+//     console.log("true")
+// }
+// else{
+//     console.log("false");
+// }
+
+// 22. Print Floobyte Intern, as many times as the number stored in the variable num, every time on a new line
+
+// let num = prompt("num");
+// let num = 5
+// let i = 0;
+// while(num>i){
+//     console.log("Floobyte Intern \n");
+//     i++; 
+// }
+  
+
+// 23.
+// let num = 5
+// let i = 0;
+// while(num>i){
+//     console.log("Hello world \n");
+//     i++; 
+// }
+
+
+//24.
+// let num = 10
+// let i = 0;
+// while(num>i){
+//     console.log("Hello world \n");
+//     i++; 
+// }
+
+// 25.Loop 1 to 5
+
+// let num = 5;
+// for(let i = 1;i<=num;i++){
+//   console.log(i);
+// }
+ 
+
+//26.<-----------
+// let num = 10;
+// for(let i = 1;i<=num;i++){
+//   console.log(i);
+// }
+
+//27.<-----------
+// let num = 5;
+// for(let i = 1;i<=num;i++){
+//   console.log(i);
+// }
+
+
+// 28. <--------------
+
+
+// let max = prompt("max");
+// let min = prompt("min");
+
+// for(let i = min;i<max;i++){
+//   console.log(i);
+// }
+
+// 29. Multiplication table of 2
+
+// let n = 2;
+// for(let i = 1;i<=10;i++){
+//   console.log(n*i);
+// }
+
+// 29. Multiplication table of 3
+
+// let n = 3;
+// for(let i = 1;i<=10;i++){
+//   console.log(n*i);
+// }
+  
+
+// 30. Multiplication of any number n 
+
+// let n = prompt("n");
+// for(let i = 1;i<=10;i++){
+//   console.log(n*i);
+// }
+
+// 31. Move 2 ahead  or print odd numbers <-----------------
+
+
+// let num = 12;
+// for(let i =1;i<=num;i++){
+//   if(i%2==1){
+//     console.log(i);
+//   }
+// }
+
+// 32. print even numbers <------------------
+
+
+// let num = 12
+// for(let i = 1;i<=num;i++){
+//   if(i%2==0){
+//     console.log(i)
+//   }
+// }
+
+// 33.sum 1 to N <-------------
+
+let num = 5;
+let sum = 0;
+
+for(let i = 0;i<=num;i++){
+  sum += i;
+}
+console.log(sum)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
